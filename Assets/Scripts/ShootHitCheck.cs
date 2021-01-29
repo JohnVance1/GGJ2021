d3a0a425b676c1a32a61811705b4dcff4ba78e70
knh190 <knh190@hotmail.com>
2021-01-30 00:56:54 +0800

    update: update stats of jump & move.

--- Assets/Scripts/ShootHitCheck.cs
+++ Assets/Scripts/ShootHitCheck.cs
@@ -1,11 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
+// @author Assy
+// attached to a bullet and check bullet hit.
 using UnityEngine;
 
 public class ShootHitCheck : MonoBehaviour
 {
     public bool HitFlag = false;
 
+    // todo this should check what it hits: player? enemy? wall?
     private void OnCollisionEnter2D(Collision2D collision)
     {
         HitFlag = true;

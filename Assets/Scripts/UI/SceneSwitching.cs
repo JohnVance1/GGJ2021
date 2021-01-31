5098b33b4fe43592a7dcb7910e11ec49fd7869cf
006sion800 <47321504+006sion800@users.noreply.github.com>
2021-01-31 23:26:09 +0900

    Title Scene (#23)
    * Began title scene creation, need to finish educational video before attempting
    
    * Began scripting for Title Selection and also layout
    
    * Began title scene creation, need to finish educational video before attempting
    
    * Began scripting for Title Selection and also layout
    
    * Trying to get the fixed main code
    
    * Got the title screen buttons working, need assets next and confirmed game scene to switch to
    
    * Implementing the title
    
    * update scene.
    
    Co-authored-by: Wren B <lxb7132@rit.edu>
    Co-authored-by: knh190 <knh190@hotmail.com>

---
+++ Assets/Scripts/UI/SceneSwitching.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitching : MonoBehaviour
+{
+    public void GameScene()
+    {
+        Debug.Log("mapData");
+        //Scene should be whatever the game is played in
+        SceneManager.LoadScene("mapData");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
